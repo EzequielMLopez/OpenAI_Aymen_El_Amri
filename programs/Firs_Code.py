aa46243dfceb1df04ca8aa11587d1b00221d59d6
Ezequiel López <EzeMaxLop@hotmail.com>
2023-11-29 11:11:10 -0300

    Correcciones

--- programs/Firs_Code.py
+++ programs/Firs_Code.py
@@ -4,25 +4,14 @@ from openai.types import Model
 import openai
 from openai import OpenAI
 
-def init_api():
-    client = OpenAI(api_key=os.environ.get("API_KEY"))
-    return client
-# Reading variables from .env file, namely API_KEY and ORG_ID
-#with open(".env") as env:
-#    for line in env:
-#        key, value= line.strip().split("=")
-#        os.environ[key]= value
-        
-# Initializing the API key and organization ID
-
-# TODO: The 'openai.organization' option isn't read in the client API. You will need to pass it when you instantiate the client, e.g. 'OpenAI(organization=os.environ.get("ORG_ID"))'
-# openai.organization= os.environ.get("ORG_ID")
+client = OpenAI(
+  api_key=os.environ['API_KEY'],  # this is also the default, it can be omitted
+)
 
 # Calling the API and listing models
-client= init_api()
-models = client.models.list()
+modeles = client.models.list()
 
 i = 0
-while(i < len(models.data)):
-    print(models.data[i].id)
+while(i < len(modeles.data)):
+    print(modeles.data[i].id)
     i += 1
